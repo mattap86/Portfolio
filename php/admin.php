e3d178454c85c0c0122492df5a1b41b44670c4cb
Matt <matthew.tapley@hotmail.co.uk>
2018-10-02 16:42:32 +0100

    added post functionality from admin.php to edit.php, put dbconn into a function, created a validate function

--- php/admin.php
+++ php/admin.php
@@ -1,49 +1,55 @@
-<!DOCTYPE html>
+
+<?php
+
+require '../php/edit.php';
+
+?>
+
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>CMS Admin</title>
 </head>
 <body>
-    <form action="../php/edit.php" method="post">
+    <form action="edit.php" method="post">
         <h1>UPDATE ABOUT ME</h1>
         <div>
             <label>Bio :</label>
-            <textarea name="update_bio" rows="10" cols="100" maxlength="600" placeholder="Max 600 characters..."></textarea>
+            <textarea name="newBio" rows="10" cols="100" maxlength="600"><?php echo $bio ?></textarea>
             <input type="submit" value="Update Biography" />
         </div>
         <div>
             <label>Update Interests :</label>
-            <textarea name="update_interests" rows="10" cols="100" maxlength="600" placeholder="Max 600 characters..."></textarea>
+            <textarea name="newInterests" rows="10" cols="100" maxlength="600"><?php echo $interests ?></textarea>
             <input type="submit" value="Update Interests" />
         </div>
         <div>
             <label>Update Qualifications :</label>
-            <textarea name="update_qualifications" rows="10" cols="100" maxlength="600" placeholder="Max 600 characters..."></textarea>
+            <textarea name="newQualifications" rows="10" cols="100" maxlength="600"><?php echo $qualifications ?></textarea>
             <input type="submit" value="Update Qualifications" />
         </div>
         <div>
             <h1>CREATE / REMOVE PROJECT</h1>
             <label>New Project</label>
-            <input type="radio" name="add_delete_project" checked />
+            <input type="radio" name="addDeleteProject" checked />
             <label>Delete Project</label>
-            <input type="radio" name="add_delete_project" />
+            <input type="radio" name="addDeleteProject" />
         </div>
         <div>
             <label>Project Name :</label>
-            <input type="text" name="new_project_name" placeholder="Enter Name Of New Project..." />
+            <input type="text" name="newProjectName" placeholder="Enter Name Of New Project..." />
         </div>
         <div>
             <label>Upload Project Image :</label>
-            <input type="text" name="new_image_url" placeholder="Enter Image Url..." />
+            <input type="text" name="newImageUrl" placeholder="Enter Image Url..." />
         </div>
         <div>
             <label>Upload Project :</label>
-            <input type="text" name="new_project_url" placeholder="Enter Project Url" />
+            <input type="text" name="newProjectUrl" placeholder="Enter Project Url" />
         </div>
         <div>
             <label>Project Summary :</label>
-            <textarea name="new_project_summary" rows="10" cols="100" maxlength="600" placeholder="Max 600 characters..."></textarea>
+            <textarea name="newProjectSummary" rows="10" cols="100" maxlength="600" placeholder="Max 600 characters..."></textarea>
         </div>
     </form>
 </body>
--- php/edit.php
+++ php/edit.php
@@ -1,43 +1,25 @@
 <?php
 
-$db = new PDO('mysql:host=127.0.0.1;dbname=portfolio', 'root');
+require 'functions.php';
 
-$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+$db = dbconn();
 
-$update_bio = $_POST['update_bio'];
-$update_interests = $_POST['update_interests'];
-$update_qualifications = $_POST['update_qualifications'];
+$updatedBio = $_POST['newBio'];
+$updatedInterests = $_POST['newInterests'];
+$updatedQualifications = $_POST['newQualifications'];
 
-//this needs putting into a function
-$update_about_me = $db->prepare("UPDATE `about_me` SET (`bio`=:bio, `interests`=:interests, `qualifications`=:qualifications) WHERE id=1");
+var_dump($updatedBio);
+//$validatedBio = validateString($updatedBio);
+//$validatedInterests = validateString($updatedInterests);
+//$validatedQualifications = validateString($updatedQualifications);
+//
+//updateAboutMe($updatedBio, $updatedInterests, $updatedQualifications, $db);
 
-$bio = filter_var($bio, FILTER_SANITIZE_STRING);
-$interests = filter_var($interests, FILTER_SANITIZE_STRING);
-$qualifications = filter_var($qualifications, FILTER_SANITIZE_STRING);
 
-$update_about_me->bindParam(':bio',$bio);
-$update_about_me->bindParam(':interests',$interests);
-$update_about_me->bindParam(':qualifications',$qualifications);
+$dbResult = getDbAboutMe($db);
 
-$update_about_me->execute();
+$bio = selectBioFromResults($dbResult);
+$interests = selectInterestsFromResults($dbResult);
+$qualifications = selectQualificationsFromResults($dbResult);
 
-if (isset($bio)) {
-    echo "Your Biography Has Been Updated";
-} else if (isset($interests)) {
-    echo "Your Interests Have Been Updated";
-} else if (isset($qualifications)) {
-    echo "Your Qualifications Have Been Updated";
-} else if(!isset($bio, $interests, $qualifications)) {
-    echo "Nothing Was Updated";
-}
-
-//$project_name_query->execute();
-//$project_image_query->execute();
-//$project_url_query->execute();
-//$project_summary_query->execute();
-
-//$project_name_query = $db->prepare("SELECT `projects`(`name`) FROM `name`;");
-//$project_image_query = $db->prepare("SELECT `projects`(`image_url`) FROM `image_url`;");
-//$project_url_query = $db->prepare("SELECT `projects`(`url`) FROM (:url);");
-//$project_summary_query = $db->prepare("SELECT `projects`(`summary`) FROM `summary`;");
 
