df3c61fe06f187caf2cac95f3c297fa4695efd4f
Matt <matthew.tapley@hotmail.co.uk>
2018-10-04 16:12:59 +0100

    added login page

---
+++ php/cmsLogin.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+    <form>
+        <input type="text" name="name" placeholder="enter username" />
+        <input type="text" name="password" placeholder="enter password" />
+        <input type="submit" value="log in" />
+    </form>
+</body>
+</html>
+
